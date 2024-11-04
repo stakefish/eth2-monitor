22ef31e034b6f0863822f61d89ccc03a25d3eb2b
Adam Szkoda <adaszko@gmail.com>
2024-11-04 16:30:15 +0100

    Fetch beacon committees for the relevant epoch

--- beaconchain/service.go
+++ beaconchain/service.go
@@ -98,7 +98,7 @@ func (beacon *BeaconChain) GetProposerDuties(ctx context.Context, epoch phase0.E
 func (beacon *BeaconChain) GetBeaconCommitees(ctx context.Context, epoch phase0.Epoch) ([]*apiv1.BeaconCommittee, error) {
 	provider := beacon.service.(eth2client.BeaconCommitteesProvider)
 	resp, err := provider.BeaconCommittees(ctx, &api.BeaconCommitteesOpts{
-		State: "justified",
+		State: fmt.Sprintf("%d", spec.EpochLowestSlot(uint64(epoch))),
 		Epoch: &epoch,
 	})
 	if err != nil {
