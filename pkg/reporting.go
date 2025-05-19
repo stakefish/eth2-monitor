2c7157ea06bf5b72fd35473ba9bdd54b997e6132
Adam Szkoda <adaszko@gmail.com>
2025-05-19 14:14:46 +0200

    Remove unused Twitter reporting

--- pkg/reporting.go
+++ pkg/reporting.go
@@ -4,16 +4,10 @@ import (
 	"bytes"
 	"encoding/json"
 	"eth2-monitor/cmd/opts"
-	"eth2-monitor/spec"
 	"fmt"
 	"net/http"
 
-	"github.com/attestantio/go-eth2-client/spec/phase0"
-	"github.com/dghubble/go-twitter/twitter"
-	"github.com/dghubble/oauth1"
 	"github.com/rs/zerolog/log"
-	"golang.org/x/text/cases"
-	"golang.org/x/text/language"
 )
 
 func Report(format string, args ...interface{}) {
@@ -56,35 +50,4 @@ func reportToSlack(message string) {
 		log.Warn().Err(err).Msgf("http.Post failed while reporting %q; skip", message)
 	}
 	defer resp.Body.Close()
-}
-
-func TweetSlashing(reason string, slot spec.Slot, slasher phase0.ValidatorIndex, slashee spec.ValidatorIndex) {
-	message := fmt.Sprintf(`📢🔪 Slashing Alert 🔪📢
-👁️ Validator %v
-🧐 %s
-🔪 Slashed by Validator %v
-📊 Occurred at Slot %v`, slashee, cases.Title(language.English).String(reason), slasher, slot)
-	reportToTwitter(message)
-}
-
-func reportToTwitter(message string) {
-	if !(opts.Slashings.TwitterConsumerKey != "" && opts.Slashings.TwitterConsumerSecret != "" &&
-		opts.Slashings.TwitterAccessToken != "" && opts.Slashings.TwitterAccessSecret != "") {
-		return
-	}
-
-	config := oauth1.NewConfig(opts.Slashings.TwitterConsumerKey, opts.Slashings.TwitterConsumerSecret)
-	token := oauth1.NewToken(opts.Slashings.TwitterAccessToken, opts.Slashings.TwitterAccessSecret)
-
-	// OAuth1 http.Client will automatically authorize Requests
-	httpClient := config.Client(oauth1.NoContext, token)
-
-	// Twitter client
-	client := twitter.NewClient(httpClient)
-
-	// Send a Tweet
-	_, _, err := client.Statuses.Update(message, nil)
-	if err != nil {
-		log.Warn().Err(err).Msgf("client.Statuses.Update failed while posting a tweet %q; skip", message)
-	}
 }
